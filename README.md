c9d80012f4f0211c40caf9f7f15e5c8915a514f7
Mikhail Soldatkin <mikhail.soldatkin@americor.com>
2026-06-19 13:41:22 +0100

    initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# Obsidian Simple Git Plugin
+This is a plugin for Obsidian (https://obsidian.md).
+
+## API Documentation
+See https://github.com/obsidianmd/obsidian-api
+
+## Quick guide
+- Clone this repo.
+- Make sure your NodeJS is at least v16 (`node --version`).
+- Run `npm i` or `yarn` to install dependencies.
+- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
+- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
+- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+- Reload Obsidian to load the new version of your plugin.
+- Enable plugin in settings window.
+- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+
+## Releasing new releases
+- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
+- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
+- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
+- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
+- Publish the release.
+
+> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
+> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+
+
+## Improve code quality with eslint (optional)
+- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
+- To use eslint with this project, make sure to install eslint from terminal:
+  - `npm install -g eslint`
+- To use eslint to analyze this project use this command:
+  - `eslint main.ts`
+  - eslint will then create a report with suggestions for code improvement by file and line number.
+- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
+  - `eslint .\src\`
---
+++ main.ts
@@ -0,0 +1,89 @@
+import {Plugin, TFile} from 'obsidian';
+import CreatingBranchModal from './src/frontend/creating-branch-modal';
+import CommittingSettingsModal from './src/frontend/committing-modal';
+import SettingsTab from './src/frontend/settings-tab';
+import {ObsidianSimpleGitPluginSettingsInterface, DEFAULT_SETTINGS} from './src/frontend/settings-tab';
+import Backend from './src/backend';
+
+export default class ObsidianSimpleGitPlugin extends Plugin {
+	settings: ObsidianSimpleGitPluginSettingsInterface;
+	backend: Backend;
+
+	async onload() {
+		await this.loadSettings();
+		this.backend = Backend.getInstance(this);
+
+		this.addSettingTab(new SettingsTab(this.app, this));
+
+		const gitLeftStatusBarItem = this.addStatusBarItem();
+		gitLeftStatusBarItem.setText('Git actions:');
+
+		const updateFromBitbucketStatusBarItem = this.addStatusBarItem();
+		updateFromBitbucketStatusBarItem.setText('↙');
+		updateFromBitbucketStatusBarItem.setAttribute("title", "Pull updates from the remote repository");
+		updateFromBitbucketStatusBarItem.classList.add("status-bar-button");
+		updateFromBitbucketStatusBarItem.addEventListener("click", () => {
+			this.backend.gitWrapper.pull().then(() => this.sendGitRefreshEvent());
+		});
+
+		const createNewBranchStatusBarItem = this.addStatusBarItem();
+		createNewBranchStatusBarItem.setText('⎇');
+		createNewBranchStatusBarItem.setAttribute("title", "Create a new branch");
+		createNewBranchStatusBarItem.classList.add("status-bar-button");
+		createNewBranchStatusBarItem.addEventListener("click", () => {
+			new CreatingBranchModal(this.app, (result) => {
+				this.backend.gitWrapper.pullAndCreateBranch(
+					this.backend.getNewBranchName(result.branchType, result.taskCode)
+				).then(() => this.sendGitRefreshEvent());
+			}).open();
+		});
+
+		const commitChangesStatusBarItem = this.addStatusBarItem();
+		commitChangesStatusBarItem.setText('⛁');
+		commitChangesStatusBarItem.setAttribute("title", "Commit all the files changes");
+		commitChangesStatusBarItem.classList.add("status-bar-button");
+		commitChangesStatusBarItem.addEventListener("click", () => {
+			new CommittingSettingsModal(this.app, 'Committing changes', "Commit", (result) => {
+				this.backend.saveCommitMessage(result.commitMessage);
+				const staging = result.autoStaging ? this.backend.gitWrapper.stageAll() : Promise.resolve();
+				staging
+					.then(() => this.backend.gitWrapper.commit(result.commitMessage))
+					.then(() => this.sendGitRefreshEvent());
+			}).open();
+		});
+
+		const uploadToBitbucketStatusBarItem = this.addStatusBarItem();
+		uploadToBitbucketStatusBarItem.setText('↗');
+		uploadToBitbucketStatusBarItem.setAttribute("title", "Push changes to the remote repository");
+		uploadToBitbucketStatusBarItem.classList.add("status-bar-button");
+		uploadToBitbucketStatusBarItem.addEventListener("click", () => {
+			new CommittingSettingsModal(this.app, 'Pushing changes', "Push", (result) => {
+				this.backend.saveCommitMessage(result.commitMessage);
+				const staging = result.autoStaging ? this.backend.gitWrapper.stageAll() : Promise.resolve();
+				staging
+					.then(() => this.backend.gitWrapper.commit(result.commitMessage))
+					.then(() => this.backend.gitWrapper.push())
+					.then(() => this.sendGitRefreshEvent());
+			}).open();
+		});
+
+		const gitRightStatusBarItem = this.addStatusBarItem();
+		gitRightStatusBarItem.setText('   ');
+	}
+
+	onunload() {
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	async sendGitRefreshEvent() {
+		const GIT_REFRESH_EVENT_NAME =  "obsidian-git:refresh";
+		this.app.workspace.trigger(GIT_REFRESH_EVENT_NAME);
+	}
+}
